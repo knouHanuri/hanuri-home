26597c28ec06ede443356b6df6572721ddaf18bc
hrJang <redblood778@gmail.com>
2024-10-19 17:05:04 +0900

    중간 저장

--- src/main/java/hanuri/website/dto/Subject.java
+++ src/main/java/hanuri/website/dto/Subject.java
@@ -1,58 +1,60 @@
 package hanuri.website.dto;
 
 public class Subject {
-    private Long seq;
-    private String id;
-    private String name;
-    private String gender;
-    private String phoneNumber;
-    private int grade;
+    private int subjectCode;    // 과목 코드
+    private String subjectName; // 과목명
+    private byte grade;         // 학년
+    private byte semester;      // 학기
+    private String professor;   // 담당 교수명
+    private boolean studyOpened; // 과목 개설 여부
 
-    public Long getSeq() {
-        return seq;
+
+    // getter와 setter
+    public int getSubjectCode() {
+        return subjectCode;
     }
 
-    public void setSeq(Long seq) {
-        this.seq = seq;
+    public void setSubjectCode(int subjectCode) {
+        this.subjectCode = subjectCode;
     }
 
-    public String getId() {
-        return id;
+    public String getSubjectName() {
+        return subjectName;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public int getGrade() {
+    public byte getGrade() {
         return grade;
     }
 
-    public void setGrade(int grade) {
+    public void setGrade(byte grade) {
         this.grade = grade;
+    }
+
+    public byte getSemester() {
+        return semester;
+    }
+
+    public void setSemester(byte semester) {
+        this.semester = semester;
+    }
+
+    public String getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(String professor) {
+        this.professor = professor;
+    }
+
+    public boolean isStudyOpened() {
+        return studyOpened;
+    }
+
+    public void setStudyOpened(boolean studyOpened) {
+        this.studyOpened = studyOpened;
     }
 }
--- src/main/resources/sql/ddl.sql
+++ src/main/resources/sql/ddl.sql
@@ -1,4 +1,86 @@
-## STUDY_HISTORY
+## MEMBER
+
+CREATE TABLE IF NOT EXISTS MEMBER (
+
+                                      member_id int(10) not null primary key,
+
+                                      username varchar(50) not null,
+
+                                      email varchar(50) not null,
+
+                                      name varchar(50) not null,
+
+                                      phone_Number varchar(15) not null,
+
+                                      password varchar(255) not null,
+
+                                      gender ENUM('male','female','other'),
+
+                                      birthdate DATE,
+
+                                      student_id varchar(20),
+
+                                      image_id int,
+
+                                      enrollment_status ENUM ('enroalled','supended','graudated'),        membership_level ENUM ('admin','full','associate','guest'),        is_active bool
+
+);
+
+## SUBJECT
+
+CREATE TABLE IF NOT EXISTS SUBJECT (
+
+                                       subject_code int PRIMARY KEY auto_increment,
+
+                                       subject_name VARCHAR(100) not null,
+
+                                       grade TINYINT ,
+
+                                       semester TINYINT ,
+
+                                       professor VARCHAR(50) ,
+
+                                       study_opened BOOLEAN not null
+
+);
+
+## STUDY
+
+CREATE TABLE IF NOT EXISTS STUDY (
+
+                                     study_id int PRIMARY KEY auto_increment,
+
+                                     subject_code int,
+
+                                     title VARCHAR(100),
+
+                                     status ENUM('active', 'completed', 'pending'),
+
+                                     schedule VARCHAR(50),
+
+                                     start_date DATE,
+
+                                     end_date DATE,
+
+                                     Established_by VARCHAR(10),
+
+                                     foreign key (subject_code) references SUBJECT (subject_code)
+
+);
+
+## **MembershipFeePayment**
+
+create table if not exists MembershipFeePayment
+(
+    member_id int not null ,
+    payment_date date not null,
+    amount int not null,
+    expiration_date date not null,
+    CONSTRAINT fk_member_id foreign key (member_id) references member(member_id)
+        on update cascade
+);
+
+## **STUDY_HISTORY**
 
 CREATE TABLE IF NOT EXISTS STUDY_HISTORY(
 
@@ -14,6 +96,8 @@ CREATE TABLE IF NOT EXISTS STUDY_HISTORY(
 
                                             notes VARCHAR(1000),
 
-                                            PRIMARY KEY (study_id, week)
+                                            PRIMARY KEY (study_id, week),
+
+                                            CONSTRAINT fk_study_id foreign key (study_id) references STUDY(study_id) on DELETE CASCADE
 
 );
