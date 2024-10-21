ffea578b2141c581e4b0da624e6f8b0cf13155ec
jyjin <jjy3386@gmail.com>
2024-10-21 18:46:40 +0900

    롬복 추가 Mybatis snakeCase -> camelCase 변환 설정 추가 log4jdbc 추가

--- src/main/java/hanuri/website/controller/MemberForm.java
+++ src/main/java/hanuri/website/controller/MemberForm.java
@@ -1,49 +1,15 @@
 package hanuri.website.controller;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
 public class MemberForm {
     private String id;
     private String name;
     private String gender;
     private String phoneNumber;
     private String grade;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getGrade() {
-        return grade;
-    }
-
-    public void setGrade(String grade) {
-        this.grade = grade;
-    }
 }
