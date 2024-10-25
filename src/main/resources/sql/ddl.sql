8722fad4d3cd9d0f9f7fa5ea0179d11b3d8e16f3
jyjin <jjy3386@gmail.com>
2024-10-25 18:19:07 +0900

    member CRUD

--- src/main/resources/sql/ddl.sql
+++ src/main/resources/sql/ddl.sql
@@ -1,10 +1,15 @@
-create table member(
-	seq INT not null auto_increment,
-	id varchar(20) not null,
-	name varchar(50) not null,
-	gender char(1),
-	phone_number varchar(11),
-	grade int,
-	constraint member_pk primary key(seq)
-
+CREATE TABLE IF NOT EXISTS MEMBER (
+    member_id int(10) primary key auto_increment,
+    username varchar(50) not null,
+    email varchar(50) not null,
+    name varchar(50) not null,
+    phone_number varchar(15) not null,
+    password varchar(255) not null,
+    gender ENUM('male','female','other'),
+    birthdate DATE,
+    student_id varchar(20),
+    image_id int,
+    enrollment_status ENUM ('enrolled','supended','graudated'),
+    membership_level ENUM ('admin','normal','guest'),
+    is_active BOOL
 );
