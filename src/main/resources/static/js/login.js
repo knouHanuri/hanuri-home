2ec3eaed5262df1f1a876dca9bc230f92da0f79d
jyjin <jjy3386@gmail.com>
2025-01-02 21:00:49 +0900

    1월2일 중간커밋

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,28 @@
+const profileImage = document.getElementById('profileImage');
+const profileMenu = document.getElementById('profile-menu');
+
+
+if(profileImage !== null){
+    profileImage.addEventListener('click', () => {
+        profileMenu.style.display = profileMenu.style.display === 'block' ? 'none' : 'block';
+    });
+
+    document.addEventListener('click', (event) => {
+        if (!event.target.closest('.profile-control')) {
+            profileMenu.style.display = 'none';
+        }
+    });
+}
+
+const hamMenuImage = document.getElementById('hamManuImage');
+const hamMenu = document.getElementById('ham-menu');
+
+hamMenuImage.addEventListener('click', () => {
+    hamMenu.style.display = hamMenu.style.display === 'none' ? 'block' : 'none';
+});
+
+document.addEventListener('click', (event) => {
+    if (!event.target.closest('.ham-menu-control')) {
+        hamMenu.style.display = 'none';
+    }
+});
